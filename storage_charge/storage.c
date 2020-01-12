171318654ed1219af9168a62b6a57b8bd661a3b9
horance <horance@outlook.com>
2020-01-12 18:57:21 +0800

    extract variable: tenant->leases[index] -> lease

--- storage_charge/storage.c
+++ storage_charge/storage.c
@@ -8,40 +8,41 @@ void charge(const Tenant* tenant, double* total, int* levels)
 {
     for (int index = 0; index < tenant->numOfLeases && index < MAX_NUM_LEASE; index++)
     {
+        const Lease* lease = tenant->leases[index];
         double price = 0;
 
-        switch (tenant->leases[index]->storage->type)
+        switch (lease->storage->type)
         {
         case ST_BLOCK_STORAGE:
             price += 40;
-            if (tenant->leases[index]->storage->capacity > BASIC_BLOCK_SIZE)
+            if (lease->storage->capacity > BASIC_BLOCK_SIZE)
             {
-                double exceed = tenant->leases[index]->storage->capacity - BASIC_BLOCK_SIZE;
-                price += tenant->leases[index]->months * exceed * 3;
+                double exceed = lease->storage->capacity - BASIC_BLOCK_SIZE;
+                price += lease->months * exceed * 3;
             }
             break;
         case ST_FILE_STORAGE:
             price += 20;
-            if (tenant->leases[index]->months > BASIC_FILE_STORAGE_MONTHS)
+            if (lease->months > BASIC_FILE_STORAGE_MONTHS)
             {
-                double exceed = tenant->leases[index]->months - BASIC_FILE_STORAGE_MONTHS;
+                double exceed = lease->months - BASIC_FILE_STORAGE_MONTHS;
                 price += exceed * 1.5;
             }
             break;
         case ST_OBJECT_STORAGE:
             price += 10;
-            if (tenant->leases[index]->months > BASIC_OBJECT_STORAG_MONTHS)
+            if (lease->months > BASIC_OBJECT_STORAG_MONTHS)
             {
-                double exceed = tenant->leases[index]->months - BASIC_OBJECT_STORAG_MONTHS;
-                price += exceed * tenant->leases[index]->storage->capacity * 1.5;
+                double exceed = lease->months - BASIC_OBJECT_STORAG_MONTHS;
+                price += exceed * lease->storage->capacity * 1.5;
             }
             break;
         default:
             break;
         }
 
-        if ((tenant->leases[index]->storage->type == ST_OBJECT_STORAGE)
-                && (tenant->leases[index]->months > MONTHS_OF_YEAR))
+        if ((lease->storage->type == ST_OBJECT_STORAGE)
+                && (lease->months > MONTHS_OF_YEAR))
         {
             *levels += 1;
         }
