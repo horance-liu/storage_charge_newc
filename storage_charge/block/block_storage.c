fb8ab79cd164ace5653e8bd34d3e3dccb175d07b
horance <horance@outlook.com>
2020-01-12 20:10:56 +0800

    setup module: block_storage.h/block_storage.c

---
+++ storage_charge/block/block_storage.c
@@ -0,0 +1,1 @@
+#include "storage_charge/block/block_storage.h"
---
+++ storage_charge/block/block_storage.h
@@ -0,0 +1,12 @@
+#ifndef H79A635D2_DDF7_4C95_B79C_F0936D9A8723
+#define H79A635D2_DDF7_4C95_B79C_F0936D9A8723
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* H79A635D2_DDF7_4C95_B79C_F0936D9A8723 */
