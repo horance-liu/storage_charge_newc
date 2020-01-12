6a22d638860c4268b373caa35e9100ef10d81fe5
horance <horance@outlook.com>
2020-01-12 21:03:33 +0800

    extract module: storage_type.h

--- storage_charge/storage.h
+++ storage_charge/storage.h
@@ -1,19 +1,11 @@
 #ifndef HAC6DA0FC_9308_4D5E_913B_AF818970CE7D
 #define HAC6DA0FC_9308_4D5E_913B_AF818970CE7D
 
+#include "storage_charge/storage_type.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/**
- * 存储资源类型
- * */
-typedef enum
-{
-    ST_BLOCK_STORAGE,  /* 块存储 */
-    ST_FILE_STORAGE,   /* 文件存储 */
-    ST_OBJECT_STORAGE, /* 对象存储 */
-} StorageType;
 
 /**
  * 存储资源
---
+++ storage_charge/storage_type.h
@@ -0,0 +1,22 @@
+#ifndef H2DE8D622_A421_49B6_8855_64C387E68DB5
+#define H2DE8D622_A421_49B6_8855_64C387E68DB5
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * 存储资源类型
+ * */
+typedef enum StorageType
+{
+    ST_BLOCK_STORAGE,  /* 块存储 */
+    ST_FILE_STORAGE,   /* 文件存储 */
+    ST_OBJECT_STORAGE, /* 对象存储 */
+} StorageType;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* H2DE8D622_A421_49B6_8855_64C387E68DB5 */
