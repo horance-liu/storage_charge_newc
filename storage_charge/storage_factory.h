20832ddebed3627b843e19bc1d12ba0199195d0b
horance <horance@outlook.com>
2020-01-12 20:51:55 +0800

    setup module: storage_factory.h/storage_factory.c

---
+++ storage_charge/storage_factory.h
@@ -0,0 +1,12 @@
+#ifndef H9523F021_5F43_4E38_ABED_F858B95921E0
+#define H9523F021_5F43_4E38_ABED_F858B95921E0
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* H9523F021_5F43_4E38_ABED_F858B95921E0 */
