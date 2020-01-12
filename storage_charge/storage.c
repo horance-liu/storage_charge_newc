21f5974af38e0832b0a4039cdfa5663495db207b
horance <horance@outlook.com>
2020-01-12 19:21:26 +0800

    inline function: level_for_lease -> lease_level

--- storage_charge/storage.c
+++ storage_charge/storage.c
@@ -1,17 +1,12 @@
 #include "storage_charge/storage.h"
 #include "storage_charge/lease.h"
 
-static int level_for_lease(const Lease* lease)
-{
-    return lease_level(lease);
-}
-
 void charge(const Tenant* tenant, double* total, int* levels)
 {
     for (int i = 0; i < tenant->numOfLeases && i < MAX_NUM_LEASE; i++)
     {
         const Lease* lease = tenant->leases[i];
-        *levels += level_for_lease(lease);
+        *levels += lease_level(lease);
         *total += lease_charge(lease);
     }
 }
