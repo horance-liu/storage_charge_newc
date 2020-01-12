c772f6f0a931d314bceb8662a8bd5c4f1d8ffeba
horance <horance@outlook.com>
2020-01-12 19:24:21 +0800

    move function: lease_charge -> charge_for_storage

--- storage_charge/lease.c
+++ storage_charge/lease.c
@@ -5,39 +5,44 @@
 #define BASIC_FILE_STORAGE_MONTHS 2
 #define BASIC_OBJECT_STORAG_MONTHS 3
 
-double lease_charge(const Lease *lease)
+static double charge_for_storage(const Storage* storage, int months)
 {
     double price = 0;
-    switch (lease->storage->type)
+    switch (storage->type)
     {
     case ST_BLOCK_STORAGE:
         price += 40;
-        if (lease->storage->capacity > BASIC_BLOCK_SIZE)
+        if (storage->capacity > BASIC_BLOCK_SIZE)
         {
-            double exceed = lease->storage->capacity - BASIC_BLOCK_SIZE;
-            price += lease->months * exceed * 3;
+            double exceed = storage->capacity - BASIC_BLOCK_SIZE;
+            price += months * exceed * 3;
         }
         break;
     case ST_FILE_STORAGE:
         price += 20;
-        if (lease->months > BASIC_FILE_STORAGE_MONTHS)
+        if (months > BASIC_FILE_STORAGE_MONTHS)
         {
-            double exceed = lease->months - BASIC_FILE_STORAGE_MONTHS;
+            double exceed = months - BASIC_FILE_STORAGE_MONTHS;
             price += exceed * 1.5;
         }
         break;
     case ST_OBJECT_STORAGE:
         price += 10;
-        if (lease->months > BASIC_OBJECT_STORAG_MONTHS)
+        if (months > BASIC_OBJECT_STORAG_MONTHS)
         {
-            double exceed = lease->months - BASIC_OBJECT_STORAG_MONTHS;
-            price += exceed * lease->storage->capacity * 1.5;
+            double exceed = months - BASIC_OBJECT_STORAG_MONTHS;
+            price += exceed * storage->capacity * 1.5;
         }
         break;
     default:
         break;
     }
     return price;
+}
+
+double lease_charge(const Lease *lease)
+{
+    return charge_for_storage(lease->storage, lease->months);
 }
 
 #define MONTHS_OF_YEAR 12
