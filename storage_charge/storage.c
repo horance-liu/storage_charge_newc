b52536d8a65b8028e5ace9feb6b5656c4a91739e
horance <horance@outlook.com>
2020-01-12 20:50:10 +0800

    extract function: storage_level_default

--- storage_charge/storage.c
+++ storage_charge/storage.c
@@ -33,13 +33,18 @@ double storage_charge(const Storage* storage, int months)
 
 typedef int (*StorageLevel)(int months);
 
+static int storage_level_default(int months)
+{
+    return 0;
+}
+
 static StorageLevel create_storage_level(StorageType type)
 {
-    return type == ST_OBJECT_STORAGE ? object_storage_level : 0;
+    return type == ST_OBJECT_STORAGE ? object_storage_level : storage_level_default;
 }
 
 int storage_level(const Storage* storage, int months)
 {
     StorageLevel level = create_storage_level(storage->type);
-    return level != 0 ? level(months) : 0;
+    return level(months);
 }
