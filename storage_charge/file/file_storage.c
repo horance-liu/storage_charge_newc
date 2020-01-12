e7eca27fe9824e3b2a6e3c6f47bc7a7a5c63bcd8
horance <horance@outlook.com>
2020-01-12 20:14:34 +0800

    setup module: file_storage.h/file_storage.c

---
+++ storage_charge/file/file_storage.c
@@ -0,0 +1,1 @@
+#include "storage_charge/file/file_storage.h"
---
+++ storage_charge/file/file_storage.h
@@ -0,0 +1,12 @@
+#ifndef HA5140D6A_43BE_4AA3_9496_833CE7647748
+#define HA5140D6A_43BE_4AA3_9496_833CE7647748
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HA5140D6A_43BE_4AA3_9496_833CE7647748 */
