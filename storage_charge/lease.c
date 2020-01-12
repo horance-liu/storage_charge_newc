0daeb657f565997d68dc6691827cd1e69aac780c
horance <horance@outlook.com>
2020-01-12 19:38:52 +0800

    inline function: storage_level -> level_for_storage

--- storage_charge/lease.c
+++ storage_charge/lease.c
@@ -6,12 +6,7 @@ double lease_charge(const Lease *lease)
     return storage_charge(lease->storage, lease->months);
 }
 
-static int level_for_storage(const Storage* storage, int months)
-{
-    return storage_level(storage, months);
-}
-
 int lease_level(const Lease *lease)
 {
-    return level_for_storage(lease->storage, lease->months);
+    return storage_level(lease->storage, lease->months);
 }
