5853a814b9a16898c2f2fa93e04e790f4297e419
horance <horance@outlook.com>
2020-01-12 19:08:39 +0800

    setup new module: lease.h/lease.c

---
+++ storage_charge/lease.h
@@ -0,0 +1,12 @@
+#ifndef H80651FA6_9F2F_4E32_A8EC_9F5E9FD153A4
+#define H80651FA6_9F2F_4E32_A8EC_9F5E9FD153A4
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* H80651FA6_9F2F_4E32_A8EC_9F5E9FD153A4 */
