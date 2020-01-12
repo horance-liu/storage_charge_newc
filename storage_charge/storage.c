c62003d3e2fec3b8578c8830e60dc98e0a0ee538
horance <horance@outlook.com>
2020-01-12 20:33:49 +0800

    extract factory method: create_storage_charge

--- storage_charge/storage.c
+++ storage_charge/storage.c
@@ -3,19 +3,27 @@
 #include "storage_charge/file/file_storage.h"
 #include "storage_charge/object/object_storage.h"
 
-double storage_charge(const Storage* storage, int months)
+typedef double (*StorageCharge)(int capacity, int months);
+
+static StorageCharge create_storage_charge(StorageType type)
 {
-    switch (storage->type)
+    switch (type)
     {
     case ST_BLOCK_STORAGE:
-        return block_storage_charge(storage->capacity, months);
+        return block_storage_charge;
     case ST_FILE_STORAGE:
-        return file_storage_charge(storage->capacity, months);
+        return file_storage_charge;
     case ST_OBJECT_STORAGE:
-        return object_storage_charge(storage->capacity, months);
+        return object_storage_charge;
     default:
         return 0;
     }
+}
+
+double storage_charge(const Storage* storage, int months)
+{
+    StorageCharge charge = create_storage_charge(storage->type);
+    return charge != 0 ? charge(storage->capacity, months) : 0;
 }
 
 int storage_level(const Storage* storage, int months)
